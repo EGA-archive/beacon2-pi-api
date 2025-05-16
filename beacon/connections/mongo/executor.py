a5c5cab29abe6b7eb95d4f0805701053a1a82d54
costero-e <orildsaga@gmail.com>
2025-05-16 09:38:55 +0200

    changing entry type variables from conf

--- beacon/connections/mongo/executor.py
+++ beacon/connections/mongo/executor.py
@@ -12,6 +12,7 @@ from beacon.connections.mongo.datasets import get_analyses_of_dataset, get_biosa
 from beacon.connections.mongo.cohorts import get_analyses_of_cohort, get_biosamples_of_cohort, get_individuals_of_cohort, get_variants_of_cohort, get_runs_of_cohort
 from beacon.connections.mongo.datasets import get_full_datasets, get_dataset_with_id
 from beacon.connections.mongo.cohorts import get_cohorts, get_cohort_with_id
+from beacon.conf import analysis, biosample, cohort, dataset as dtaset, genomicVariant, individual, run
 
 @log_with_args(level)
 async def execute_function(self, entry_type: str, datasets: list, qparams: RequestParams, entry_id: Optional[str]):
@@ -21,70 +22,70 @@ async def execute_function(self, entry_type: str, datasets: list, qparams: Reque
     datasets_count={}
     new_count=0
     if entry_id == None:
-        if entry_type == 'g_variants':
+        if entry_type == genomicVariant.endpoint_name:
             function=get_variants
-        elif entry_type == 'individuals':
+        elif entry_type == individual.endpoint_name:
             function=get_individuals
-        elif entry_type == 'analyses':
+        elif entry_type == analysis.endpoint_name:
             function=get_analyses
-        elif entry_type == 'biosamples':
+        elif entry_type == biosample.endpoint_name:
             function=get_biosamples
-        elif entry_type == 'runs':
+        elif entry_type == run.endpoint_name:
             function=get_runs
     else:
-        if entry_type == 'g_variants':
+        if entry_type == genomicVariant.endpoint_name:
             function=get_variant_with_id
-        elif entry_type == 'individuals':
+        elif entry_type == individual.endpoint_name:
             function=get_individual_with_id
-        elif entry_type == 'analyses':
+        elif entry_type == analysis.endpoint_name:
             function=get_analysis_with_id
-        elif entry_type == 'biosamples':
+        elif entry_type == biosample.endpoint_name:
             function=get_biosample_with_id
-        elif entry_type == 'runs':
+        elif entry_type == run.endpoint_name:
             function=get_run_with_id
-        elif entry_type == 'g_variants_biosamples':
+        elif entry_type == genomicVariant.endpoint_name + '_' + biosample.endpoint_name:
             function = get_biosamples_of_variant
-        elif entry_type == 'g_variants_analyses':
+        elif entry_type == genomicVariant.endpoint_name + '_' + analysis.endpoint_name:
             function = get_analyses_of_variant
-        elif entry_type == 'g_variants_individuals':
+        elif entry_type == genomicVariant.endpoint_name + '_' + individual.endpoint_name:
             function = get_individuals_of_variant
-        elif entry_type == 'g_variants_runs':
+        elif entry_type == genomicVariant.endpoint_name + '_' + run.endpoint_name:
             function = get_runs_of_variant
-        elif entry_type == 'analyses_g_variants':
+        elif entry_type == analysis.endpoint_name + '_' + genomicVariant.endpoint_name:
             function = get_variants_of_analysis
-        elif entry_type == 'biosamples_g_variants':
+        elif entry_type == biosample.endpoint_name + '_' + genomicVariant.endpoint_name:
             function = get_variants_of_biosample
-        elif entry_type == 'biosamples_analyses':
+        elif entry_type == biosample.endpoint_name + '_' + analysis.endpoint_name:
             function = get_analyses_of_biosample
-        elif entry_type == 'biosamples_runs':
+        elif entry_type == biosample.endpoint_name + '_' + run.endpoint_name:
             function = get_runs_of_biosample
-        elif entry_type == 'individuals_biosamples':
+        elif entry_type == individual.endpoint_name + '_' + biosample.endpoint_name:
             function = get_biosamples_of_individual
-        elif entry_type == 'individuals_g_variants':
+        elif entry_type == individual.endpoint_name + '_' + genomicVariant.endpoint_name:
             function = get_variants_of_individual
-        elif entry_type == 'runs_analyses':
+        elif entry_type == run.endpoint_name + '_' + analysis.endpoint_name:
             function = get_analyses_of_run
-        elif entry_type == 'runs_g_variants':
+        elif entry_type == run.endpoint_name + '_' + genomicVariant.endpoint_name:
             function = get_variants_of_run
-        elif entry_type == 'datasets_analyses':
+        elif entry_type == dtaset.endpoint_name + '_' + analysis.endpoint_name:
             function=get_analyses_of_dataset
-        elif entry_type == 'datasets_biosamples':
+        elif entry_type == dtaset.endpoint_name + '_' + biosample.endpoint_name:
             function=get_biosamples_of_dataset
-        elif entry_type == 'datasets_individuals':
+        elif entry_type == dtaset.endpoint_name + '_' + individual.endpoint_name:
             function=get_individuals_of_dataset
-        elif entry_type == 'datasets_g_variants':
+        elif entry_type == dtaset.endpoint_name + '_' + genomicVariant.endpoint_name:
             function=get_variants_of_dataset
-        elif entry_type == 'datasets_runs':
+        elif entry_type == dtaset.endpoint_name + '_' + run.endpoint_name:
             function=get_runs_of_dataset
-        elif entry_type == 'cohorts_analyses':
+        elif entry_type == cohort.endpoint_name + '_' + analysis.endpoint_name:
             function=get_analyses_of_cohort
-        elif entry_type == 'cohorts_biosamples':
+        elif entry_type == cohort.endpoint_name + '_' + biosample.endpoint_name:
             function=get_biosamples_of_cohort
-        elif entry_type == 'cohorts_individuals':
+        elif entry_type == cohort.endpoint_name + '_' + individual.endpoint_name:
             function=get_individuals_of_cohort
-        elif entry_type == 'cohorts_g_variants':
+        elif entry_type == cohort.endpoint_name + '_' + genomicVariant.endpoint_name:
             function=get_variants_of_cohort
-        elif entry_type == 'cohorts_runs':
+        elif entry_type == cohort.endpoint_name + '_' + run.endpoint_name:
             function=get_runs_of_cohort
 
     loop = asyncio.get_running_loop()
@@ -137,14 +138,14 @@ async def execute_function(self, entry_type: str, datasets: list, qparams: Reque
 @log_with_args(level)
 async def execute_collection_function(self, entry_type: str, qparams: RequestParams, entry_id: Optional[str]):
     if entry_id == None:
-        if entry_type == 'datasets':
+        if entry_type == dtaset.endpoint_name:
             function=get_full_datasets
-        elif entry_type == 'cohorts':
+        elif entry_type == cohort.endpoint_name:
             function=get_cohorts
     else:
-        if entry_type == 'datasets':
+        if entry_type == dtaset.endpoint_name:
             function=get_dataset_with_id
-        elif entry_type == 'cohorts':
+        elif entry_type == cohort.endpoint_name:
             function=get_cohort_with_id
     response_converted, count, entity_schema = function(self, entry_id, qparams)
     return response_converted, count, entity_schema
