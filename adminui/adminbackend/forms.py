2d3fa9fe03ee4e31232f5d7d8a3b657386b576de
costero-e <orildsaga@gmail.com>
2025-05-23 17:35:14 +0200

    adding more fields in the form

--- adminui/adminbackend/forms.py
+++ adminui/adminbackend/forms.py
@@ -1,13 +1,15 @@
 from django import forms
+import yaml
 
 class BamForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(BamForm, self).__init__(*args, **kwargs)
         # assign a (computed, I assume) default value to the choice field
+        self.fields['APIVersion'].widget.attrs['readonly'] = True
+        self.fields['BeaconVersion'].widget.attrs['readonly'] = True
         with open("adminui/beacon/conf/conf.py") as f:
             lines = f.readlines()
         with open("adminui/beacon/conf/conf.py", "r") as f:
-            new_lines =''
             for line in lines:
                 if 'beacon_name' in str(line):
                     linestring=str(line)
@@ -42,8 +44,25 @@ class BamForm(forms.Form):
                         placeholder=placeholder_def[0]
                         placeholder=placeholder.strip()
                     self.initial['Environment'] = placeholder
+                elif 'api_version=' in str(line) or 'api_version =' in str(line):
+                    linestring=str(line)
+                    splitted_line=linestring.split("=")
+                    placeholder=splitted_line[1].replace("'", '')
+                    if isinstance(placeholder, str):
+                        placeholder.strip()
+                    if "#" in placeholder:
+                        placeholder_def=placeholder.split('#')
+                        placeholder=placeholder_def[0]
+                        placeholder=placeholder.strip()
+                    self.initial['APIVersion'] = placeholder
+                elif str(line).startswith('version'):
+                    with open("beacon/conf/api_version.yml") as api_version_file:
+                        api_version_yaml = yaml.safe_load(api_version_file)
+                    self.initial['BeaconVersion'] = api_version_yaml['api_version']
          
     BeaconName = forms.CharField(help_text='Beacon Name')
     BeaconId = forms.CharField(help_text='Beacon Id')
     environment_choices = [("TEST", "TEST"), ("DEV", "DEV"), ("PROD", "PROD")]
     Environment = forms.ChoiceField(choices=environment_choices, help_text="Environment")
+    APIVersion = forms.CharField(help_text='API Version')
+    BeaconVersion = forms.CharField(help_text='Beacon Version')
