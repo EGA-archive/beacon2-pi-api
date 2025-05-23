b60bd65813c11b01ce1fe9a1c74307aa1e052cd8
costero-e <orildsaga@gmail.com>
2025-05-23 11:05:31 +0200

    adding htmx

--- adminui/adminclient/views.py
+++ adminui/adminclient/views.py
@@ -1,38 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-import subprocess
-import time
 from django.http import HttpResponseRedirect, HttpResponseBadRequest
 import logging
-import json
 from pymongo.mongo_client import MongoClient
-import imp
 from django.urls import resolve
 from adminbackend.forms import BamForm
-import subprocess
-import sys
 from beacon.connections.mongo import conf
 from beacon.conf import conf as basic_conf
 
-client = MongoClient(
-        "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
-            conf.database_user,
-            conf.database_password,
-            conf.database_host,
-            conf.database_port,
-            conf.database_name,
-            conf.database_auth_source,
-        )
-    )
-
-
-
-
-LOG = logging.getLogger(__name__)
-
 def default_view(request):
     form =BamForm()
-    context = {'form': form, "beacon_name": basic_conf.beacon_name}
+    context = {'form': form}
     if request.method == 'POST':
         form = BamForm(request.POST)
         if form.is_valid():
