6e138a2f5c8471502fa2d78a6b6d21399aa6a8b3
costero-e <orildsaga@gmail.com>
2025-06-05 10:48:22 +0200

    adding pipelines for ri-tools

--- ri-tools/conf/conf.py
+++ ri-tools/conf/conf.py
@@ -3,13 +3,13 @@ csv_folder = './csv/examples/test/'
 output_docs_folder='./output_docs/'
 
 #### VCF Conversion config parameters ####
-allele_counts=False
+allele_counts=False # Variable still in test, leave it as False for now.
 reference_genome='GRCh37' # Choose one between NCBI36, GRCh37, GRCh38
 datasetId='test'
 case_level_data=True
-exact_heterozygosity=False
-num_rows=7000000
-verbosity=False
+exact_heterozygosity=True
+num_rows=15000000
+verbosity=False # This variable, if True, will make the program run slower but give logs about all the skipped variants and the reason why.
 
 ### MongoDB parameters ###
 database_host = 'mongo'
