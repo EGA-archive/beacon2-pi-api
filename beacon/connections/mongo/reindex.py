3fe379571ac749b6c704ce901b019b4213a0e51d
costero-e <orildsaga@gmail.com>
2025-07-17 15:33:05 +0200

    adding unique indexes to avoid duplication

--- beacon/connections/mongo/reindex.py
+++ beacon/connections/mongo/reindex.py
@@ -1,4 +1,4 @@
-from beacon.connections.mongo.__init__ import client, dbname, genomicVariations, caseLevelData
+from beacon.connections.mongo.__init__ import client, dbname, genomicVariations, caseLevelData, individuals, analyses, biosamples, cohorts, datasets, runs, targets
 
 try:
     client[dbname].drop_collection("synonyms")
@@ -34,11 +34,18 @@ except Exception:
     db=client[dbname].create_collection(name="similarities")
 
 genomicVariations.create_index([("length", 1), ("variation.location.interval.start.value", 1),("variation.location.interval.end.value", 1)]) # for range queries
-genomicVariations.create_index([("variation.alternateBases", 1),("variation.referenceBases", 1),("variation.location.interval.start.value", 1), ("variation.location.interval.end.value", 1)]) # for sequence queries
+#genomicVariations.create_index([("variation.alternateBases", 1),("variation.referenceBases", 1),("variation.location.interval.start.value", 1), ("variation.location.interval.end.value", 1)]) # for sequence queries
+genomicVariations.create_index([("variation.alternateBases", 1),("variation.referenceBases", 1),("variation.location.interval.start.value", 1), ("variation.location.interval.end.value", 1), ("datasetId", 1)], unique=True) # for sequence queries
 genomicVariations.create_index([("datasetId", 1)]) # splits all the docs into datasets faster
 genomicVariations.create_index([("variantInternalId", 1)]) # enables the g_variants/{id}/endpoint query to do it faster
 genomicVariations.create_index([("identifiers.genomicHGVSId", 1)])
 genomicVariations.create_index([("molecularAttributes.geneIds", 1), ("variation.variantType", 1)])
-caseLevelData.create_index([("id", 1), ("datasetId", 1)])
+caseLevelData.create_index([("id", 1), ("datasetId", 1)], unique=True)
 caseLevelData.create_index([("datasetId", 1)])
-
+individuals.create_index([("id", 1), ("datasetId", 1)], unique=True)
+biosamples.create_index([("id", 1), ("datasetId", 1)], unique=True)
+analyses.create_index([("id", 1), ("datasetId", 1)], unique=True)
+cohorts.create_index([("id", 1), ("datasetId", 1)], unique=True)
+runs.create_index([("id", 1), ("datasetId", 1)], unique=True)
+datasets.create_index([("id", 1), ("datasetId", 1)], unique=True)
+targets.create_index([("datasetId", 1)], unique=True)
