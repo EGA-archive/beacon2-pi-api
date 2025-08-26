bbec1287a9b7144292f6a4290a51f6c681a96196
costero-e <orildsaga@gmail.com>
2025-08-26 17:53:30 +0200

    identity providers implemented in admin UI

--- adminui/adminclient/urls.py
+++ adminui/adminclient/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from .views import HomePageView
-from .views import views, datasets, filtering_terms, synonyms, descendants, permits, budget, rounding_counts, connections, service_status
+from .views import views, datasets, filtering_terms, synonyms, descendants, permits, budget, rounding_counts, connections, service_status, identity_provider
 app_name = 'adminclients'
 
 urlpatterns = [
@@ -14,5 +14,6 @@ urlpatterns = [
     path('budget', budget.default_view, name='budget'),
     path('rounding_counts', rounding_counts.default_view, name='rounding_counts'),
     path('connections', connections.default_view, name='connections'),
-    path('service_status', service_status.default_view, name='service_status')
+    path('service_status', service_status.default_view, name='service_status'),
+    path('identity_provider', identity_provider.default_view, name='identity_provider')
 ]
---
+++ adminui/adminclient/views/identity_provider.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+import logging
+from adminbackend.forms.identity_provider import IDPForm
+import yaml
+import glob
+import logging
+import os
+import subprocess
+from dotenv import load_dotenv, set_key
+
+LOG = logging.getLogger(__name__)
+fmt = '%(levelname)s - %(asctime)s - %(message)s'
+formatter = logging.Formatter(fmt)
+sh = logging.StreamHandler()
+sh.setLevel('NOTSET')
+sh.setFormatter(formatter)
+LOG.addHandler(sh)
+
+def default_view(request):
+    form = IDPForm()
+    
+    identity_provider_list=[]
+    
+    for env_filename in glob.glob("adminui/beacon/auth/idp_providers/*.env"):
+        load_dotenv(env_filename, override=True)
+        identity_provider_dict={}
+        env_id = env_filename.split('/')
+        env_id = env_id[-1]
+        env_id = env_id.split('.')
+        env_id = env_id[0]
+        identity_provider_dict["IdentityProvider"]=env_id
+        identity_provider_dict["Issuer"] = os.getenv('ISSUER')
+        identity_provider_dict["ClientID"] = os.getenv('CLIENT_ID')
+        identity_provider_dict["ClientSecret"] = os.getenv('CLIENT_SECRET')
+        identity_provider_dict["UserInfo"] = os.getenv('USER_INFO')
+        identity_provider_dict["Introspection"] = os.getenv('INTROSPECTION')
+        identity_provider_dict["JWKSURL"] = os.getenv('JWKS_URL')
+        identity_provider_list.append(identity_provider_dict)
+        
+    if request.method == 'POST':
+        form = IDPForm(request.POST)
+        if form.is_valid():
+            idp = form.cleaned_data['IdentityProvider']
+            issuer = form.cleaned_data['Issuer']
+            client_id = form.cleaned_data['ClientID']
+            client_secret = form.cleaned_data['ClientSecret']
+            user_info = form.cleaned_data['UserInfo']
+            introspection = form.cleaned_data['Introspection']
+            jwks_url = form.cleaned_data['JWKSURL']
+            load_dotenv("adminui/beacon/auth/idp_providers/" + idp + '.env', override=True)
+            LOG.warning(idp)
+            LOG.warning(issuer)
+            LOG.warning(client_id)
+            if 'Save' in request.POST:
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="CLIENT_ID", value_to_set=client_id)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="CLIENT_SECRET", value_to_set=client_secret)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="USER_INFO", value_to_set=user_info)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="INTROSPECTION", value_to_set=introspection)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="JWKS_URL", value_to_set=jwks_url)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="ISSUER", value_to_set=issuer)
+            elif 'Add' in request.POST:
+                create_idp = subprocess.check_output("touch adminui/beacon/auth/idp_providers/{}.env".format(idp), shell=True)
+                load_dotenv("adminui/beacon/auth/idp_providers/" + idp + '.env', override=True)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="CLIENT_ID", value_to_set=client_id)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="CLIENT_SECRET", value_to_set=client_secret)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="USER_INFO", value_to_set=user_info)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="INTROSPECTION", value_to_set=introspection)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="JWKS_URL", value_to_set=jwks_url)
+                set_key(dotenv_path="adminui/beacon/auth/idp_providers/" + idp + '.env', key_to_set="ISSUER", value_to_set=issuer)
+            elif 'Delete' in request.POST:
+                os.remove("adminui/beacon/auth/idp_providers/" + idp)
+            return redirect("adminclient:identity_provider")
+    context={"identity_provider_list": identity_provider_list ,"form": form}
+    template = "general_configuration/identity_provider.html"
+    return render(request, template, context)
