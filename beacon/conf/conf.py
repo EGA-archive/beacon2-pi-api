457748fad055c4a1034b7ad189da3a2dfc116bb9
costero-e <orildsaga@gmail.com>
2025-10-20 14:58:56 +0200

    adding new conf

--- beacon/conf/conf.py
+++ beacon/conf/conf.py
@@ -11,23 +11,23 @@ except Exception as e:
 
 level=logging.NOTSET
 log_file=None
-beacon_id = 'org.ega-archive.beacon-ri-demo'  # ID of the Beacon
-beacon_name = 'Beacon Production Implementation demo'  # Name of the Beacon service
+beacon_id = 'org.ega-archive.gdi-spain-beacon'  # ID of the Beacon
+beacon_name = 'GDI Spain Beacon'  # Name of the Beacon service
 api_version = 'v2.0.0' # Version of the Beacon implementation
-uri = 'http://localhost:5050'
+uri = 'https://beacon-spain.ega-archive.org'
 uri_subpath = '/api'
-complete_url = uri + uri_subpath
 environment = 'test'
-description = r"This Beacon is based on synthetic data hosted at the <a href='https://ega-archive.org/datasets/EGAD00001003338'>EGA</a>. The dataset contains 2504 samples including genetic data based on 1K Genomes data, and 76 individual attributes and phenotypic data derived from UKBiobank."
+description = r"This Beacon is based on synthetic data hosted at GDI Spain Node. It includes three datasets: the B1MG one million genomes, 2504 samples from CINECA UK1 synthetic dataset and the rare diseases dataset from rd-connect."
 version = api_version_yaml['api_version']
 welcome_url = 'https://beacon.ega-archive.org/'
-alternative_url = 'https://beacon.ega-archive.org/api'
+alternative_url = 'https://beacon-spain.ega-archive.org/api'
 create_datetime = '2021-11-29T12:00:00.000000'
 update_datetime = ''
 default_beacon_granularity = "record" # boolean, count or record
 security_levels = ['PUBLIC', 'REGISTERED', 'CONTROLLED']
 documentation_url = 'https://b2ri-documentation-demo.ega-archive.org/'
-cors_urls = ["http://localhost:3003", "http://localhost:3000"]
+alphanumeric_terms = ['libraryStrategy', 'molecularAttributes.geneIds', 'diseases.ageOfOnset.iso8601duration', 'molecularAttributes.aminoacidChanges','phenotypicFeatures.onset.iso8601duration']
+cors_urls = ["http://localhost:3000","https://cancer-beacon-demo.ega-archive.org", "https://beacon-network-demo2.ega-archive.org", "https://beacon.ega-archive.org"]
 
 # Service Info
 ga4gh_service_type_group = 'org.ga4gh'
