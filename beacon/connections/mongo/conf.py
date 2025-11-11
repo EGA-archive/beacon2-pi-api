bb318d8475711bd9def6765ac489b9e45f7d9a49
Ivars Silamikelis <ivars.silamikelis@gmail.com>
2025-11-11 15:00:07 +0200

    fix: use MongoClient auth parameters to avoid URL encoding issues
    Fixes #120

--- beacon/connections/mongo/conf.py
+++ beacon/connections/mongo/conf.py
@@ -4,12 +4,12 @@ import os
 mongo_conf = "beacon/connections/mongo/conf.env"
 load_dotenv(mongo_conf, override=True)
 
-database_host = ''.join(envar for envar in os.getenv('database_host', 'mongo') if envar.isalnum())
-database_port = ''.join(envar for envar in os.getenv('database_port', str(27017)) if envar.isalnum())
-database_user = ''.join(envar for envar in os.getenv('database_user', 'root') if envar.isalnum())
-database_password = ''.join(envar for envar in os.getenv('database_password', 'example') if envar.isalnum())
-database_name = ''.join(envar for envar in os.getenv('database_name', 'beacon') if envar.isalnum())
-database_auth_source = ''.join(envar for envar in os.getenv('database_auth_source', 'admin') if envar.isalnum())
-database_certificate = ''.join(envar for envar in os.getenv('database_certificate', '') if envar.isalnum())
-database_cafile = ''.join(envar for envar in os.getenv('database_cafile', '') if envar.isalnum())
+database_host = os.getenv('database_host', 'mongo')
+database_port = os.getenv('database_port', str(27017))
+database_user = os.getenv('database_user', 'root')
+database_password = os.getenv('database_password', 'example')
+database_name = os.getenv('database_name', 'beacon')
+database_auth_source = os.getenv('database_auth_source', 'admin')
+database_certificate = os.getenv('database_certificate', '')
+database_cafile = os.getenv('database_cafile', '')
 database_cluster = False
