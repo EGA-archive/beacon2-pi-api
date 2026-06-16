b5893ae0dcf617abc47c36e5266817d396d9bdcc
costero-e <orildsaga@gmail.com>
2026-06-16 13:50:26 +0200

    checking comment language

--- scripts/check_comment_language.py
+++ scripts/check_comment_language.py
@@ -18,7 +18,7 @@ for py_file in target_dir.rglob("*.py"):
 
                 try:
                     if detect(text) != "en":
-                        violations.append("file: {} at line {} has next detected part of non-English: {}".format(py_file, token.start[0], text))
+                        violations.append("file: {} at line {} has next detected part of non-English: {}. Text is in language: {}".format(py_file, token.start[0], text, detect(text)))
                 except Exception:
                     pass
 
