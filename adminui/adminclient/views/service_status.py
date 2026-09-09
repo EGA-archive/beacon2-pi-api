edf19eb39d0e31a6f36e54d4e323e2e11e2f295a
costero-e <orildsaga@gmail.com>
2026-09-09 12:24:32 +0200

    finished service status

--- adminui/adminclient/views/service_status.py
+++ adminui/adminclient/views/service_status.py
@@ -6,14 +6,45 @@ import os
 import logging
 import importlib
 from django.contrib.auth.decorators import login_required, permission_required
+import requests
+from dotenv import load_dotenv, get_key, set_key
 
-LOG = logging.getLogger(__name__)
-fmt = '%(levelname)s - %(asctime)s - %(message)s'
-formatter = logging.Formatter(fmt)
-sh = logging.StreamHandler()
-sh.setLevel('NOTSET')
-sh.setFormatter(formatter)
-LOG.addHandler(sh)
+def test_api():
+    try:
+        response = requests.get(config.complete_url+'/health', timeout=5)
+        api = response.json()
+    except Exception as e:
+        api = e
+    context = {"api": api}
+    return context
+
+def test_ui():
+    try:
+        response = requests.get(config.welcome_url, timeout=5)
+        ui = response.json()
+    except Exception as e:
+        ui = e
+    context = {"ui": ui}
+    return context
+
+def test_idps():
+    path="/home/app/web/beacon/auth/idp_providers/"
+    idp_dict={}
+    files = [
+        f for f in os.listdir(path)
+        if os.path.isfile(os.path.join(path, f))
+    ]
+    for file in files:
+        print(file, flush=True)
+        if file.endswith(".env"):
+            issuer=get_key(dotenv_path=path + file, key_to_get="ISSUER")
+            well_known_endpoint=issuer+".well-known/openid-configuration"
+            try:
+                idp_dict[file]=requests.get(well_known_endpoint, timeout=5)
+            except Exception as e:
+                idp_dict[file]=e
+    context = {"idp": idp_dict}
+    return context
 
 #@login_required
 #@permission_required('adminclient.can_see_view', raise_exception=True)
@@ -21,65 +52,17 @@ def default_view(request):
     context = {}
     if request.method == 'POST':
         for k in request.POST:
-
-            if k == 'TestDatabase':
-                dirs = os.listdir("/home/app/web/beacon/connections")
-                for dir in dirs:
-                    if 'mongo' in dir:
-                        try:
-                            complete_module='beacon.connections.'+dir
-                            module = importlib.import_module(complete_module, package=None)
-                            client = module.client.server_info()
-                            client = "Ok and running in a mongo " + client["version"] + "version"
-                        except Exception:
-                            client = 'Connection could not be established'
-                context = {"client": client}
-            elif k == 'TestAPI':
-                try:
-                    client = subprocess.check_output("curl -s -o /dev/null -v {}".format(config.uri+config.uri_subpath), shell=True)
-                    client = 'Connection successful'
-                except Exception:
-                    client = 'Connection could not be established'
-                context = {"client": client}
-            elif k == 'BudgetDatabase':
-                if 'mongo' in config.query_budget_database:
-                    try:
-                        complete_module='beacon.connections.'+config.query_budget_database
-                        module = importlib.import_module(complete_module, package=None)
-                        client = module.client.server_info()
-                        client = "Ok and running in a mongo " + client["version"] + "version"
-                    except Exception:
-                        client = 'Connection could not be established'
-                context = {"client": client}
+            if k == 'TestAPI':
+                context = test_api()
             elif k == 'UserInterface':
-                pass
+                context = test_ui()
+            elif k == 'IdentityProviders':
+                context = test_idps()
             elif k == 'GeneralStatus':
-                dirs = os.listdir("/home/app/web/beacon/connections")
-                for dir in dirs:
-                    if 'mongo' in dir:
-                        try:
-                            complete_module='beacon.connections.'+dir
-                            module = importlib.import_module(complete_module, package=None)
-                            database = module.client.server_info()
-                            database = "Ok and running in a mongo " + database["version"] + "version"
-                        except Exception:
-                            database = 'Connection could not be established'
-                try:
-                    api = subprocess.check_output("curl -s -o /dev/null -v {}".format(config.uri+config.uri_subpath), shell=True)
-                    api = 'Connection successful'
-                except Exception:
-                    api = 'Connection could not be established'
-                if 'mongo' in config.query_budget_database:
-                    try:
-                        complete_module='beacon.connections.'+config.query_budget_database
-                        module = importlib.import_module(complete_module, package=None)
-                        budget = module.client.server_info()
-                        budget = "Ok and running in a mongo " + budget["version"] + "version"
-                    except Exception:
-                        budget = 'Connection could not be established'
-                context = {"database": database, "budget": budget, "api": api}
-                
-
+                context_api = test_api()
+                context_ui = test_ui()
+                context_idps = test_idps()
+                context = {**context_api, **context_ui, **context_idps}
         template = "general_configuration/service_status.html"
         return render(request, template, context)
     template = "general_configuration/service_status.html"
